984bc15ca9e9457f577485aa5fa518418028cf53
Damir Zarkesh <240088@astanait.edu.kz>
2025-01-22 11:10:37 +0500

    Damir

---
+++ src/Patient.java
@@ -0,0 +1,80 @@
+public class Patient {
+    private int id;
+    private String iin;
+    private String name;
+    private String dateOfBirth;
+    private String gender;
+    private String nationality;
+    private String citizenship;
+    private String address;
+    private String bloodGroup;
+    private String rhesusFactor;
+    private String contactInfo;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getIin() {
+        return iin;
+    }
+    public void setIin(String iin) {
+        this.iin = iin;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getNationality() {
+        return nationality;
+    }
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public String getCitizenship() {
+        return citizenship;
+    }
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+    public String getRhesusFactor() {
+        return rhesusFactor;
+    }
+    public void setRhesusFactor(String rhesusFactor) {
+        this.rhesusFactor = rhesusFactor;
+    }
+    public String getContactInfo() {
+        return contactInfo;
+    }
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+}
