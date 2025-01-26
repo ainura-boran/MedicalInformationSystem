181cd4bd1369db3520b531876b4674e77de597ed
Damir Zarkesh <240088@astanait.edu.kz>
2025-01-26 16:06:27 +0500

    Damir

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -1,6 +1,47 @@
 package models;
 
 public class Appointment {
-    //Need to think
+    public int idDoctor;
+    public int idPatient;
+    public String data;
+    public String time;
 
+    public Appointment(int idDoctor, int idPatient, String data, String time) {
+        setIdDoctor(idDoctor);
+        setIdPatient(idPatient);
+        setData(data);
+        setTime(time);
+    }
+
+    public int getIdDoctor() {
+        return idDoctor;
+    }
+
+    public void setIdDoctor(int idDoctor) {
+        this.idDoctor = idDoctor;
+    }
+
+    public int getIdPatient() {
+        return idPatient;
+    }
+
+    public void setIdPatient(int idPatient) {
+        this.idPatient = idPatient;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
