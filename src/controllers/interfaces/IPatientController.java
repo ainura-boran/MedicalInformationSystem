f0e01f2e9a914ab0f39072ceecca22d2d58ff1ab
almukhammed77 <amantaialmukhammed@gmail.com>
2025-01-27 09:29:15 +0500

    patient controller interface

--- src/controllers/interfaces/IPatientController.java
+++ src/controllers/interfaces/IPatientController.java
@@ -1,4 +1,13 @@
 package controllers.interfaces;
 
+import models.Patient;
+
+import java.util.List;
+
 public interface IPatientController {
+    boolean addPatient(Patient patient);
+
+    String getPatientById(int id);
+
+    List<Patient> getAllPatients();
 }
