dca06686cf04c06e581bc8ed1f727282920dbe59
Yerasyl <marshall2508l@gmail.com>
2025-01-28 09:08:54 +0500

    repository package

--- src/repositories/AppointmentRepository.java
+++ src/repositories/AppointmentRepository.java
@@ -1,4 +1,27 @@
-package repositories;
+import models.Appointment;
+import repositories.interfaces.IAppointmentRepository;
 
-public class AppointmentRepository {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppointmentRepository implements IAppointmentRepository {
+    private final List<Appointment> appointments = new ArrayList<>();
+
+    @Override
+    public boolean createAppointment(Appointment appointment) {
+        return appointments.add(appointment);
+    }
+
+    @Override
+    public Appointment getAppointmentById(int id) {
+        return appointments.stream()
+                .filter(appointment -> appointment.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Appointment> getAllAppointments() {
+        return new ArrayList<>(appointments);
+    }
 }
