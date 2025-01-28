d262dcd6a70aaebdadf26db15b22bf2abf900451
Yerasyl <marshall2508l@gmail.com>
2025-01-28 09:11:53 +0500

    repository package

--- src/repositories/PatientRepository.java
+++ src/repositories/PatientRepository.java
@@ -1,4 +1,29 @@
 package repositories;
 
-public class PatientRepository {
+import models.Patient;
+import repositories.interfaces.IPatientRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientRepository implements IPatientRepository {
+    private final List<Patient> patients = new ArrayList<>();
+
+    @Override
+    public boolean createPatient(Patient patient) {
+        return patients.add(patient);
+    }
+
+    @Override
+    public Patient getPatientById(int id) {
+        return patients.stream()
+                .filter(patient -> patient.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<Patient> getAllPatients() {
+        return new ArrayList<>(patients);
+    }
 }
