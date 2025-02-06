1aea240bc1282c6159026bb46ec02f5081e402ab
Damir Zarkesh <240088@astanait.edu.kz>
2025-02-06 20:18:34 +0500

    Damir

--- src/Main.java
+++ src/Main.java
@@ -22,55 +22,11 @@ public class Main {
 
         PostgresDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "12345", "hospital");
 
-//        DoctorRepository doctorRepository = new DoctorRepository(db);
-//        PatientRepository patientRepository = new PatientRepository(db);
-//        AppointmentRepository appointmentRepository = new AppointmentRepository(db);
-//        AdminRepository adminRepository = new AdminRepository(db);
-//
-//        DoctorController doctorController = new DoctorController(doctorRepository);
-//        PatientController patientController = new PatientController(patientRepository);
-//        AppointmentController appointmentController = new AppointmentController(appointmentRepository, doctorRepository.getAllDoctors(), patientRepository.getAllPatients());
-//
-//        DoctorApplication doctorApplication = new DoctorApplication(doctorController, appointmentController, patientRepository);
-//        PatientApplication patientApplication = new PatientApplication(patientController, appointmentController, doctorRepository);
-//        AdminApplication adminApplication = new AdminApplication(doctorApplication, patientApplication, appointmentController, adminRepository, appointmentRepository);
 
         Scanner scanner = new Scanner(System.in);
         HospitalApplication app = new HospitalApplication();
         app.start();
         db.close();
-//        while (true) {
-//            System.out.println("-----Who are you?-----");
-//            System.out.println("1. Doctor");
-//            System.out.println("2. Patient");
-//            System.out.println("3. Administrator");
-//            System.out.println("0. Exit");
-//            System.out.print("Enter your choice: ");
-//
-//            try {
-//                int option = scanner.nextInt();
-//                scanner.nextLine();
-//
-//                switch (option) {
-//                    case 1:
-//                        doctorApplication.start(scanner);
-//                        break;
-//                    case 2:
-//                        patientApplication.start();
-//                        break;
-//                    case 3:
-//                        adminApplication.start(scanner);
-//                        break;
-//                    case 0:
-//                        System.out.println("Exiting...");
-//                        return;
-//                    default:
-//                        System.out.println("Invalid option. Try again.");
-//                }
-//            } catch (Exception e) {
-//                System.out.println("Invalid input. Please try again.");
-//                scanner.nextLine();
-//            }
-//        }
+
     }
 }
