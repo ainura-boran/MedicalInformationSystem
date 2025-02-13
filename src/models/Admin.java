ce213d916a425dd2be181574dedcfa4a8dbbf05d
Damir Zarkesh <240088@astanait.edu.kz>
2025-02-14 02:44:05 +0500

    Damir

--- src/models/Admin.java
+++ src/models/Admin.java
@@ -1,13 +1,42 @@
 package models;
 
-public class Admin extends User {
+public class Admin {
+    private int id;
+    private String username;
+    private String passwordHash;
 
-    public Admin(int id, String fullName, String username, String passwordHash) {
-        super(id, fullName, username, passwordHash);
+    private Admin(Builder builder) {
+        this.id = builder.id;
+        this.username = builder.username;
+        this.passwordHash = builder.passwordHash;
     }
 
-    @Override
-    public String getRole() {
-        return "Admin";
+    public static class Builder {
+        private int id;
+        private String username;
+        private String passwordHash;
+
+        public Builder setId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPasswordHash(String passwordHash) {
+            this.passwordHash = passwordHash;
+            return this;
+        }
+
+        public Admin build() {
+            return new Admin(this);
+        }
     }
+
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getPasswordHash() { return passwordHash; }
 }
