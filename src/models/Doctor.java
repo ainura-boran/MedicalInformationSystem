d61f71b8c8c28da415d77558e7ef1e0dc494cd1e
Damir Zarkesh <240088@astanait.edu.kz>
2025-02-14 02:37:05 +0500

    Damir

--- src/models/Doctor.java
+++ src/models/Doctor.java
@@ -4,48 +4,25 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Doctor {
-    private int id;
-    private String fullName;
+public class Doctor extends User {
     private String specialization;
     private String workingHours;
     private String office;
     private int experienceYears;
-    private String username;
-    private String password;
     private List<Appointment> appointments = new ArrayList<>();
 
-    public Doctor() {}
-
-    public Doctor(int id, String fullName, String specialization, String workingHours, String office, int experienceYears, String username, String password) {
-        this.id = id;
-        this.fullName = fullName;
+    public Doctor(int id, String fullName, String username, String passwordHash,
+                  String specialization, String workingHours, String office, int experienceYears) {
+        super(id, fullName, username, passwordHash);
         this.specialization = specialization;
         this.workingHours = workingHours;
         this.office = office;
         this.experienceYears = experienceYears;
-        this.username = username;
-        this.password = password;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
+    @Override
+    public String getRole() {
+        return "Doctor";
     }
 
     public String getSpecialization() {
@@ -64,23 +41,16 @@ public class Doctor {
         return experienceYears;
     }
 
-    @Override
-    public String toString() {
-        return "Doctor {" +
-                "ID: " + id +
-                ", Full Name: '" + fullName + '\'' +
-                ", Specialization: '" + specialization + '\'' +
-                ", Working Hours: '" + workingHours + '\'' +
-                ", Office: '" + office + '\'' +
-                ", Experience: " + experienceYears + " years" +
-                '}';
-    }
-
     public boolean isAvailable(LocalDateTime dateTime) {
         return appointments.stream().noneMatch(a -> a.getDateTime().equals(dateTime));
     }
 
     public void addAppointment(Appointment appointment) {
         appointments.add(appointment);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " { Specialization: " + specialization + ", Office: " + office + " }";
     }
 }
